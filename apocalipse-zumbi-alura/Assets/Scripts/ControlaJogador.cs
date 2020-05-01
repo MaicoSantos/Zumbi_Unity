675555de806ae1ec3b7a4e66c3944645ffde9885
Maico Santos Salvo <mizuharamaico@gmail.com>
2020-05-01 14:08:33 -0300

    first commit

---
+++ apocalipse-zumbi-alura/Assets/Scripts/ControlaJogador.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaJogador : MonoBehaviour
+{
+    public float Velocidade = 10;
+    Vector3 direcao;
+
+    void Update()
+    {
+        float eixo_X = Input.GetAxis("Horizontal");
+        float eixo_Z = Input.GetAxis("Vertical");
+
+        direcao = new Vector3(eixo_X, 0, eixo_Z);
+
+
+        // transform.Translate(direcao * Velocidade * Time.deltaTime);
+
+        if (direcao != Vector3.zero)
+        {
+            GetComponent<Animator>().SetBool("Movendo", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Movendo", false);
+        }
+    }
+
+     void FixedUpdate() {
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + 
+        (direcao * Velocidade * Time.deltaTime));
+    }
+}
